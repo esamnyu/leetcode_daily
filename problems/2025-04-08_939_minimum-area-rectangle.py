e9b470cc56124fc527d8e768ab884307da33c574
GitHub Action <action@github.com>
2025-04-08 20:26:31 +0000

    Daily LeetCode challenge for 2025-04-08

---
+++ problems/2025-04-08_939_minimum-area-rectangle.py
@@ -0,0 +1,40 @@
+# LeetCode 939: Minimum Area Rectangle
+# Date: 2025-04-08
+# Difficulty: Medium
+# Topics: Array, Hash Table, Math, Geometry, Sorting
+
+'''
+You are given an array of points in the X-Y plane points where points[i] = [xi, yi].
+Return the minimum area of a rectangle formed from these points, with sides parallel to the X and Y axes. If there is not any such rectangle, return 0.
+ 
+Example 1:
+
+
+Input: points = [[1,1],[1,3],[3,1],[3,3],[2,2]]
+Output: 4
+
+Example 2:
+
+
+Input: points = [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
+Output: 2
+
+ 
+Constraints:
+
+1 <= points.length <= 500
+points[i].length == 2
+0 <= xi, yi <= 4 * 104
+All the given points are unique.
+
+
+'''
+
+class Solution(object):
+    def minAreaRect(self, points):
+        # TODO: Implement your solution here
+        pass
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        
