36f1c037cb928d3edfdaac340009a3ca12bc1182
GitHub Action <action@github.com>
2025-04-15 01:12:58 +0000

    Daily LeetCode challenge for 2025-04-15

---
+++ problems/2025-04-15_1124_longest-well-performing-interval.py
@@ -0,0 +1,39 @@
+# LeetCode 1124: Longest Well-Performing Interval
+# Date: 2025-04-15
+# Difficulty: Medium
+# Topics: Array, Hash Table, Stack, Monotonic Stack, Prefix Sum
+
+'''
+We are given hours, a list of the number of hours worked per day for a given employee.
+A day is considered to be a tiring day if and only if the number of hours worked is (strictly) greater than 8.
+A well-performing interval is an interval of days for which the number of tiring days is strictly larger than the number of non-tiring days.
+Return the length of the longest well-performing interval.
+ 
+Example 1:
+
+Input: hours = [9,9,6,0,6,6,9]
+Output: 3
+Explanation: The longest well-performing interval is [9,9,6].
+
+Example 2:
+
+Input: hours = [6,6,6]
+Output: 0
+
+ 
+Constraints:
+
+1 <= hours.length <= 104
+0 <= hours[i] <= 16
+
+
+'''
+
+class Solution(object):
+    def longestWPI(self, hours):
+        # TODO: Implement your solution here
+        pass
+        :type hours: List[int]
+        :rtype: int
+        """
+        
