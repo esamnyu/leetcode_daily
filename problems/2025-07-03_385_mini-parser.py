aa8b805dc543ec5bbc9f0a683019ff1af0b670fb
GitHub Action <action@github.com>
2025-07-03 01:17:37 +0000

    Daily LeetCode challenge for 2025-07-03

---
+++ problems/2025-07-03_385_mini-parser.py
@@ -0,0 +1,88 @@
+# LeetCode 385: Mini Parser
+# Date: 2025-07-03
+# Difficulty: Medium
+# Topics: String, Stack, Depth-First Search
+
+'''
+Given a string s represents the serialization of a nested list, implement a parser to deserialize it and return the deserialized NestedInteger.
+Each element is either an integer or a list whose elements may also be integers or other lists.
+ 
+Example 1:
+
+Input: s = "324"
+Output: 324
+Explanation: You should return a NestedInteger object which contains a single integer 324.
+
+Example 2:
+
+Input: s = "[123,[456,[789]]]"
+Output: [123,[456,[789]]]
+Explanation: Return a NestedInteger object containing a nested list with 2 elements:
+1. An integer containing value 123.
+2. A nested list containing two elements:
+    i.  An integer containing value 456.
+    ii. A nested list with one element:
+         a. An integer containing value 789
+
+ 
+Constraints:
+
+1 <= s.length <= 5 * 104
+s consists of digits, square brackets "[]", negative sign '-', and commas ','.
+s is the serialization of valid NestedInteger.
+All the values in the input are in the range [-106, 106].
+
+
+'''
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+return None  # TODO: Implement your solution
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+return None  # TODO: Implement your solution
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+return None  # TODO: Implement your solution
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def deserialize(self, s):
+        # TODO: Implement your solution here
+        pass
+        :type s: str
+        :rtype: NestedInteger
+        """
+        
