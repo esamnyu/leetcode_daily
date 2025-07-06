df3064b10981ea180ecf9e5f42103e3960e68715
GitHub Action <action@github.com>
2025-07-06 01:24:25 +0000

    Daily LeetCode challenge for 2025-07-06

---
+++ problems/2025-07-06_508_most-frequent-subtree-sum.py
@@ -0,0 +1,44 @@
+# LeetCode 508: Most Frequent Subtree Sum
+# Date: 2025-07-06
+# Difficulty: Medium
+# Topics: Hash Table, Tree, Depth-First Search, Binary Tree
+
+'''
+Given the root of a binary tree, return the most frequent subtree sum. If there is a tie, return all the values with the highest frequency in any order.
+The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself).
+ 
+Example 1:
+
+
+Input: root = [5,2,-3]
+Output: [2,-3,4]
+
+Example 2:
+
+
+Input: root = [5,2,-5]
+Output: [2]
+
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-105 <= Node.val <= 105
+
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findFrequentTreeSum(self, root):
+        # TODO: Implement your solution here
+        pass
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        
