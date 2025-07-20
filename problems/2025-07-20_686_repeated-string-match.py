c3ba19d867778390ca1e9d3d6f44274b2429dc3a
esamnyu <es5888@nyu.edu>
2025-07-20 05:16:23 +0000

    feat: Start LeetCode Repeated string match (2025-07-20)

---
+++ problems/2025-07-20_686_repeated-string-match.py
@@ -0,0 +1,38 @@
+# LeetCode 686: Repeated String Match
+# Date: 2025-07-20
+# Difficulty: Medium
+# Topics: String, String Matching
+
+'''
+Given two strings a and b, return the minimum number of times you should repeat string a so that string b is a substring of it. If it is impossible for b​​​​​​ to be a substring of a after repeating it, return -1.
+Notice: string "abc" repeated 0 times is "", repeated 1 time is "abc" and repeated 2 times is "abcabc".
+ 
+Example 1:
+
+Input: a = "abcd", b = "cdabcdab"
+Output: 3
+Explanation: We return 3 because by repeating a three times "abcdabcdabcd", b is a substring of it.
+
+Example 2:
+
+Input: a = "a", b = "aa"
+Output: 2
+
+ 
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist of lowercase English letters.
+
+
+'''
+
+class Solution(object):
+    def repeatedStringMatch(self, a, b):
+        # TODO: Implement your solution here
+        pass
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        
