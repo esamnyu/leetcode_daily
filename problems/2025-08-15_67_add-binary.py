a3d4609fe6351f78d37613e01b19a7e0b731b198
GitHub Action <action@github.com>
2025-08-15 01:18:37 +0000

    Daily LeetCode challenge for 2025-08-15

---
+++ problems/2025-08-15_67_add-binary.py
@@ -0,0 +1,34 @@
+# LeetCode 67: Add Binary
+# Date: 2025-08-15
+# Difficulty: Easy
+# Topics: Math, String, Bit Manipulation, Simulation
+
+'''
+Given two binary strings a and b, return their sum as a binary string.
+ 
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+ 
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+
+
+'''
+
+class Solution(object):
+    def addBinary(self, a, b):
+        # TODO: Implement your solution here
+        pass
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        
