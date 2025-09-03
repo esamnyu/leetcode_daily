9a3c2cf62eaa3f92559a39bcedbd6b190e1f7b10
GitHub Action <action@github.com>
2025-09-03 01:08:01 +0000

    Daily LeetCode challenge for 2025-09-03

---
+++ problems/2025-09-03_143_reorder-list.py
@@ -0,0 +1,50 @@
+# LeetCode 143: Reorder List
+# Date: 2025-09-03
+# Difficulty: Medium
+# Topics: Linked List, Two Pointers, Stack, Recursion
+
+'''
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+ 
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+
+ 
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        # TODO: Implement your solution here
+        pass
+        :type head: Optional[ListNode]
+        return None  # TODO: Implement your solution
+        """
+        
