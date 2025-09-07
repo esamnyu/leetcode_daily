0c2dd4bf080249c94b7a23996725d2c6323897b2
esamnyu <es5888@nyu.edu>
2025-09-07 05:08:40 +0000

    feat: Start LeetCode Rotating the box (2025-09-07)

---
+++ problems/2025-09-07_1861_rotating-the-box.py
@@ -0,0 +1,66 @@
+# LeetCode 1861: Rotating the Box
+# Date: 2025-09-07
+# Difficulty: Medium
+# Topics: Array, Two Pointers, Matrix
+
+'''
+You are given an m x n matrix of characters boxGrid representing a side-view of a box. Each cell of the box is one of the following:
+
+A stone '#'
+A stationary obstacle '*'
+Empty '.'
+
+The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity. Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box. Gravity does not affect the obstacles' positions, and the inertia from the box's rotation does not affect the stones' horizontal positions.
+It is guaranteed that each stone in boxGrid rests on an obstacle, another stone, or the bottom of the box.
+Return an n x m matrix representing the box after the rotation described above.
+ 
+Example 1:
+
+
+Input: boxGrid = [["#",".","#"]]
+Output: [["."],
+         ["#"],
+         ["#"]]
+
+Example 2:
+
+
+Input: boxGrid = [["#",".","*","."],
+              ["#","#","*","."]]
+Output: [["#","."],
+         ["#","#"],
+         ["*","*"],
+         [".","."]]
+
+Example 3:
+
+
+Input: boxGrid = [["#","#","*",".","*","."],
+              ["#","#","#","*",".","."],
+              ["#","#","#",".","#","."]]
+Output: [[".","#","#"],
+         [".","#","#"],
+         ["#","#","*"],
+         ["#","*","."],
+         ["#",".","*"],
+         ["#",".","."]]
+
+ 
+Constraints:
+
+m == boxGrid.length
+n == boxGrid[i].length
+1 <= m, n <= 500
+boxGrid[i][j] is either '#', '*', or '.'.
+
+
+'''
+
+class Solution(object):
+    def rotateTheBox(self, boxGrid):
+        # TODO: Implement your solution here
+        pass
+        :type boxGrid: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        
