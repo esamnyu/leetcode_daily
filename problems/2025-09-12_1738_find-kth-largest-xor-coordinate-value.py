ab9370bc498b804fb944fe32814ec5f28d0d2dfb
esamnyu <es5888@nyu.edu>
2025-09-12 05:08:42 +0000

    feat: Start LeetCode Find kth largest xor coordinate value (2025-09-12)

---
+++ problems/2025-09-12_1738_find-kth-largest-xor-coordinate-value.py
@@ -0,0 +1,48 @@
+# LeetCode 1738: Find Kth Largest XOR Coordinate Value
+# Date: 2025-09-12
+# Difficulty: Medium
+# Topics: Array, Divide and Conquer, Bit Manipulation, Sorting, Heap (Priority Queue), Matrix, Prefix Sum, Quickselect
+
+'''
+You are given a 2D matrix of size m x n, consisting of non-negative integers. You are also given an integer k.
+The value of coordinate (a, b) of the matrix is the XOR of all matrix[i][j] where 0 <= i <= a < m and 0 <= j <= b < n (0-indexed).
+Find the kth largest value (1-indexed) of all the coordinates of matrix.
+ 
+Example 1:
+
+Input: matrix = [[5,2],[1,6]], k = 1
+Output: 7
+Explanation: The value of coordinate (0,1) is 5 XOR 2 = 7, which is the largest value.
+
+Example 2:
+
+Input: matrix = [[5,2],[1,6]], k = 2
+Output: 5
+Explanation: The value of coordinate (0,0) is 5 = 5, which is the 2nd largest value.
+
+Example 3:
+
+Input: matrix = [[5,2],[1,6]], k = 3
+Output: 4
+Explanation: The value of coordinate (1,0) is 5 XOR 1 = 4, which is the 3rd largest value.
+ 
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+0 <= matrix[i][j] <= 106
+1 <= k <= m * n
+
+
+'''
+
+class Solution(object):
+    def kthLargestValue(self, matrix, k):
+        # TODO: Implement your solution here
+        pass
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        
