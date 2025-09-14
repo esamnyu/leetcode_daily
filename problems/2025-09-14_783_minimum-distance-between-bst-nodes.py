8bd147923e8b2da71f9dd86c1f8179061d20278e
GitHub Action <action@github.com>
2025-09-14 01:13:54 +0000

    Daily LeetCode challenge for 2025-09-14

---
+++ problems/2025-09-14_783_minimum-distance-between-bst-nodes.py
@@ -0,0 +1,45 @@
+# LeetCode 783: Minimum Distance Between BST Nodes
+# Date: 2025-09-14
+# Difficulty: Easy
+# Topics: Tree, Depth-First Search, Breadth-First Search, Binary Search Tree, Binary Tree
+
+'''
+Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+ 
+Example 1:
+
+
+Input: root = [4,2,6,1,3]
+Output: 1
+
+Example 2:
+
+
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [2, 100].
+0 <= Node.val <= 105
+
+ 
+Note: This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDiffInBST(self, root):
+        # TODO: Implement your solution here
+        pass
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        
