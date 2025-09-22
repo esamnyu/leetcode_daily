7e3dd683550388f5303c1c4d40cf0996e5e6f9d7
GitHub Action <action@github.com>
2025-09-22 01:15:14 +0000

    Daily LeetCode challenge for 2025-09-22

---
+++ problems/2025-09-22_1641_count-sorted-vowel-strings.py
@@ -0,0 +1,44 @@
+# LeetCode 1641: Count Sorted Vowel Strings
+# Date: 2025-09-22
+# Difficulty: Medium
+# Topics: Math, Dynamic Programming, Combinatorics
+
+'''
+Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+ 
+Example 1:
+
+Input: n = 1
+Output: 5
+Explanation: The 5 sorted strings that consist of vowels only are ["a","e","i","o","u"].
+
+Example 2:
+
+Input: n = 2
+Output: 15
+Explanation: The 15 sorted strings that consist of vowels only are
+["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
+Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
+
+Example 3:
+
+Input: n = 33
+Output: 66045
+
+ 
+Constraints:
+
+1 <= n <= 50 
+
+
+'''
+
+class Solution(object):
+    def countVowelStrings(self, n):
+        # TODO: Implement your solution here
+        pass
+        :type n: int
+        :rtype: int
+        """
+        
