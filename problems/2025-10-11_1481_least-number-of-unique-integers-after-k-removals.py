1ae5982f32b08546a0676276af5e55cf64a45fcf
GitHub Action <action@github.com>
2025-10-11 01:07:10 +0000

    Daily LeetCode challenge for 2025-10-11

---
+++ problems/2025-10-11_1481_least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,32 @@
+# LeetCode 1481: Least Number of Unique Integers after K Removals
+# Date: 2025-10-11
+# Difficulty: Medium
+# Topics: Array, Hash Table, Greedy, Sorting, Counting
+
+'''
+Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+
+ 
+Example 1:
+
+Input: arr = [5,5,4], k = 1
+Output: 1
+Explanation: Remove the single 4, only 5 is left.
+
+Example 2:
+
+Input: arr = [4,3,1,1,3,3,2], k = 3
+Output: 2
+Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+ 
+Constraints:
+
+1 <= arr.length <= 10^5
+1 <= arr[i] <= 10^9
+0 <= k <= arr.length
+
+'''
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
