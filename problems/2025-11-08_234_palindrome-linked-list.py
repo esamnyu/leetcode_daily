d775cdebaf7481defcca1be4200ce0437ce5c95c
esamnyu <es5888@nyu.edu>
2025-11-08 05:09:03 +0000

    feat: Start LeetCode Palindrome linked list (2025-11-08)

---
+++ problems/2025-11-08_234_palindrome-linked-list.py
@@ -0,0 +1,38 @@
+# LeetCode 234: Palindrome Linked List
+# Date: 2025-11-08
+# Difficulty: Easy
+# Topics: Linked List, Two Pointers, Stack, Recursion
+
+'''
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+ 
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+
+ 
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+
+ 
+Follow up: Could you do it in O(n) time and O(1) space?
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
