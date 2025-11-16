c7ed380153ae7972e16d3631f82bff337d9cf07e
esamnyu <es5888@nyu.edu>
2025-11-16 05:09:32 +0000

    feat: Start LeetCode Make three strings equal (2025-11-16)

---
+++ problems/2025-11-16_2937_make-three-strings-equal.py
@@ -0,0 +1,33 @@
+# LeetCode 2937: Make Three Strings Equal
+# Date: 2025-11-16
+# Difficulty: Easy
+# Topics: String
+
+'''
+You are given three strings: s1, s2, and s3. In one operation you can choose one of these strings and delete its rightmost character. Note that you cannot completely empty a string.
+Return the minimum number of operations required to make the strings equal. If it is impossible to make them equal, return -1.
+ 
+Example 1:
+
+Input: s1 = "abc", s2 = "abb", s3 = "ab"
+Output: 2
+Explanation: Deleting the rightmost character from both s1 and s2 will result in three equal strings.
+
+Example 2:
+
+Input: s1 = "dac", s2 = "bac", s3 = "cac"
+Output: -1
+Explanation: Since the first letters of s1 and s2 differ, they cannot be made equal.
+
+ 
+Constraints:
+
+1 <= s1.length, s2.length, s3.length <= 100
+s1, s2 and s3 consist only of lowercase English letters.
+
+
+'''
+
+class Solution:
+    def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
+        
