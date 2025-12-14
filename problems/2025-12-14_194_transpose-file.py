a1e12ee712ef0755366027467636d56f95a545df
GitHub Action <action@github.com>
2025-12-14 01:24:37 +0000

    Daily LeetCode challenge for 2025-12-14

---
+++ problems/2025-12-14_194_transpose-file.py
@@ -0,0 +1,26 @@
+# LeetCode 194: Transpose File
+# Date: 2025-12-14
+# Difficulty: Medium
+# Topics: Shell
+
+'''
+Given a text file file.txt, transpose its content.
+You may assume that each row has the same number of columns, and each field is separated by the ' ' character.
+Example:
+If file.txt has the following content:
+
+name age
+alice 21
+ryan 30
+
+Output the following:
+
+name alice ryan
+age 21 30
+
+
+'''
+
+# No Python template available
+
+# Implement your solution here
