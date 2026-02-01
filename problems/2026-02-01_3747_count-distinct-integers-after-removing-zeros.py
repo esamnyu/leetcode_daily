c4dd3d2f0ee88f3f0889c16960958dc4bde5f1cb
GitHub Action <action@github.com>
2026-02-01 01:59:19 +0000

    Daily LeetCode challenge for 2026-02-01

---
+++ problems/2026-02-01_3747_count-distinct-integers-after-removing-zeros.py
@@ -0,0 +1,35 @@
+# LeetCode 3747: Count Distinct Integers After Removing Zeros
+# Date: 2026-02-01
+# Difficulty: Medium
+# Topics: Math, Dynamic Programming
+
+'''
+You are given a positive integer n.
+For every integer x from 1 to n, we write down the integer obtained by removing all zeros from the decimal representation of x.
+Return an integer denoting the number of distinct integers written down.
+ 
+Example 1:
+
+Input: n = 10
+Output: 9
+Explanation:
+The integers we wrote down are 1, 2, 3, 4, 5, 6, 7, 8, 9, 1. There are 9 distinct integers (1, 2, 3, 4, 5, 6, 7, 8, 9).
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation:
+The integers we wrote down are 1, 2, 3. There are 3 distinct integers (1, 2, 3).
+
+ 
+Constraints:
+
+1 <= n <= 1015
+
+
+'''
+
+class Solution:
+    def countDistinct(self, n: int) -> int:
+        
