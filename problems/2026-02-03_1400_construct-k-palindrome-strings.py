37d67718ea1cf6ec547046f06c78b3f576243524
GitHub Action <action@github.com>
2026-02-03 01:50:46 +0000

    Daily LeetCode challenge for 2026-02-03

---
+++ problems/2026-02-03_1400_construct-k-palindrome-strings.py
@@ -0,0 +1,40 @@
+# LeetCode 1400: Construct K Palindrome Strings
+# Date: 2026-02-03
+# Difficulty: Medium
+# Topics: Hash Table, String, Greedy, Counting
+
+'''
+Given a string s and an integer k, return true if you can use all the characters in s to construct non-empty k palindrome strings or false otherwise.
+ 
+Example 1:
+
+Input: s = "annabelle", k = 2
+Output: true
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+
+Example 2:
+
+Input: s = "leetcode", k = 3
+Output: false
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+
+Example 3:
+
+Input: s = "true", k = 4
+Output: true
+Explanation: The only possible solution is to put each character in a separate string.
+
+ 
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= 105
+
+
+'''
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        
